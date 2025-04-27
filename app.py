1beefe695ab84bc704f66ff395eb4765944c7eb6
Shivani Patel <shivani@shivanismacbook.3500.dhcp.asu.edu>
2025-04-27 16:32:07 -0700

    created nav bar and playlists, profile, and search pages

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def login():
     if request.method == 'POST':
         return render_template('home.html')
-    return render_template('login.html')  # or whatever your one page is called
+    return render_template('login.html')
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -18,6 +18,18 @@ def signup():
 @app.route('/home')
 def home():
     return render_template('home.html')
+
+@app.route('/playlists')
+def playlists():
+    return render_template('playlists.html')
+
+@app.route('/search')
+def search():
+    return render_template('search.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
