0a7cf2de2e2c5512048a102fe6eb5445ee9ac4b5
Shivani Patel <shivani@shivanismacbook.3500.dhcp.asu.edu>
2025-04-27 15:51:53 -0700

    created new account form on signup page and did routing

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        return render_template('home.html')
     return render_template('login.html')  # or whatever your one page is called
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        return render_template('home.html')
     return render_template('signup.html')
 
 @app.route('/home')
