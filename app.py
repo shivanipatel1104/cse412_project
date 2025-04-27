aff509de6a07f299ceb530fdd661fff352d8e000
Shivani Patel <shivani@shivanismacbook.3500.dhcp.asu.edu>
2025-04-27 15:18:36 -0700

    created login, signup, and home pages

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
 
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def login():
+    return render_template('login.html')  # or whatever your one page is called
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
