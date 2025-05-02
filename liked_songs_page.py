99cedb59a1be1f8911639a9ac6b6dd1eb6e92f5c
Shivani Patel <shivani@shivanismacbook.3500.dhcp.asu.edu>
2025-05-02 12:10:59 -0700

    implement delete functionality on liked songs page

--- liked_songs_page.py
+++ liked_songs_page.py
@@ -15,7 +15,7 @@ def liked_songs():
 		conn = get_db_connection()
 		with conn.cursor() as cur:
 			query = """
-				SELECT s_songname, al_albumName, a_artistName
+				SELECT s_songID, s_songname, al_albumName, a_artistName
 				FROM song
 				JOIN album ON s_albumID = al_albumID
 				JOIN artist ON al_artistID = a_artistID
@@ -28,6 +28,7 @@ def liked_songs():
 			if not songs:
 				message = "No liked songs for your account."
 				return render_template('liked_songs.html', error=message)
+			
 
 	except psycopg2.DatabaseError as e:
 		message = f"Database error: {str(e)}"
@@ -43,3 +44,75 @@ def liked_songs():
 
 	return render_template('liked_songs.html', songs=songs)
 
+@liked_songs_page.route('/delete_like', methods = ['POST'])
+def delete_liked_song():
+    try:
+        song_id = request.form.get('liked_song')
+        user_id = session.get('user_id')
+
+        if not song_id:
+            return render_template('liked_songs.html', error="Error: No song session id")
+
+        if not user_id:
+            return render_template('liked_songs.html', error="Error: No user session id")
+
+        conn = get_db_connection()
+        with conn:
+            with conn.cursor() as cur:
+                # Get song name
+                song_name_query = 'SELECT s_songname FROM song WHERE s_songID = %s'
+                cur.execute(song_name_query, (song_id,))
+                result = cur.fetchone()
+
+                if result:
+                    song_name = result[0]
+
+                else:
+                    mess = 'Error: song id does not exist'
+                    return render_template('liked_songs.html', error=mess)
+
+                # Checking if song is in user's liked songs
+                query = """
+                    SELECT *
+                    FROM likedsongs
+                    WHERE l_userID = %s AND l_songID = %s
+                """
+                cur.execute(query, (user_id, song_id))
+                is_liked = cur.fetchone()
+
+                # If is in user's liked songs, then remove it
+                if is_liked:
+                    message = f"{song_name} removed from liked songs!"
+                    query = """
+                        DELETE FROM likedsongs
+                        WHERE l_userID = %s AND l_songID = %s 
+                    """
+                    cur.execute(query, (user_id, song_id))
+
+                # Fetch song data for message
+                song_data_query = """
+                    SELECT s_songname
+                    FROM song
+                    WHERE s_songID = %s
+                """
+                cur.execute(song_data_query, (song_id,))
+
+                result = cur.fetchone()
+
+                if result:
+                    song_name = result[0]
+
+                else:
+                    return render_template('liked_songs.html', error='Unknown song name')
+                
+                session['song_liked_message'] = message
+
+    except psycopg2.DatabaseError as e:
+        message = f"Database error: {str(e)}"
+        return render_template('liked_songs.html', error=message)
+
+    except Exception as e:
+        message = f'Error when handling request: {str(e)}'
+        return render_template('liked_songs.html', error=message)
+
+    return redirect(url_for('liked_songs_page.liked_songs'))
